43fe21fe4348be3490c1474d9207532b2998a35e
ksu <ksush3@mail.ru>
2022-11-25 17:34:19 +0300

    add all

--- Task2/Program.cs
+++ Task2/Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, 5 World!");
+﻿Console.Clear();
+Console.WriteLine("Введите 1-е число ");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите 2-е число ");
+int m = Convert.ToInt32(Console.ReadLine());
+int max = 0;
+int min = 0;
+if ( n > m ) 
+{
+    max = n; min = m;
+}
+ else 
+ {
+    max = m; min = n;
+ }
+Console.Write("max = ");
+Console.WriteLine(max);
+ Console.Write("min = ");
+ Console.Write(min);
+    
+
+
+    
