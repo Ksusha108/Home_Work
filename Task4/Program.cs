4f89e9248a6f0a275f8303a3b10bfd69a0c62191
ksu <ksush3@mail.ru>
2022-11-26 00:32:36 +0300

    add task 4

--- Task4/Program.cs
+++ Task4/Program.cs
@@ -5,7 +5,7 @@ Console.WriteLine("Введите 2-е число ");
 int b = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine("Введите 3-е число ");
 int c = Convert.ToInt32(Console.ReadLine());
-int max = 0;
+int max = a;
 if ( a > max ) max = a;
 if ( b > max ) max = b;
 if ( c > max ) max = c;
