1724a44c7e4dd3632ae9ea891d35b5034ba94cfd
ksu <ksush3@mail.ru>
2022-11-28 17:26:52 +0300

    add Task15

---
+++ Task15/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.Clear();
+Console.WriteLine("Введите цифру дня недели: ");
+int n = Convert.ToInt32(Console.ReadLine());
+if (n <=5 )
+    Console.WriteLine("no");
+
+ else 
+
+    Console.Write("yes");
