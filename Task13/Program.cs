9305e42c7b6c0eac28efbf69820c314349dd04ce
ksu <ksush3@mail.ru>
2022-11-28 16:00:09 +0300

    add task13

---
+++ Task13/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Clear();
+Console.WriteLine("Введите число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+if (n < 100)
+    Console.WriteLine("третьей цифры нет");
+ else 
+ {
+    while (n > 1000)
+        n = n / 10;
+    Console.Write(n % 10);
+ }
+    
+ 
+      
