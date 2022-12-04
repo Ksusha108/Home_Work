d0ce4c1359691d69b0f50c8ff9b95dfc0f8354f7
ksu <ksush3@mail.ru>
2022-12-04 19:06:50 +0300

    add task21

---
+++ Task21/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.Clear();
+Console.WriteLine("Введите координаты Х1: ");
+double x1 = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Введите координаты Y1: ");
+double y1 = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Введите координаты Z1: ");
+double z1 = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Введите координаты X2: ");
+double x2 = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Введите координаты Y2: ");
+double y2 = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Введите координаты Z2: ");
+double z2 = Convert.ToDouble(Console.ReadLine());
+
+Console.WriteLine(Math.Sqrt(Math.Pow(x1- x2, 2) + Math.Pow(y1 - y2, 2) + (Math.Pow(z1-z2, 2))));
