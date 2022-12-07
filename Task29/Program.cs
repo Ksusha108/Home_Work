f8ff0b0a751abbd8ac1b26599c61ed94fe05303b
ksu <ksush3@mail.ru>
2022-12-07 15:48:22 +0300

    add task 29

---
+++ Task29/Program.cs
@@ -0,0 +1,14 @@
+﻿// Напишите программу, которая задаёт массив из 8 элементов, 
+//предоставить возможность пользователю ввести числа и вывести их на экран.
+
+Console.Clear();
+int n = 8;
+int[] array = new int[n];
+
+for (int i = 0; i < array.Length; i++)
+{
+    Console.Write("Введите элементы массива: ");
+    int x = Convert.ToInt32(Console.ReadLine());
+    array[i] = x;
+}
+Console.WriteLine($"Исходный массив: [{string.Join(", ", array)}]");
