63c82e129edacb2fc598cefd579ba2b5b8af500c
ksu <ksush3@mail.ru>
2022-12-16 21:35:00 +0300

    add task 47

---
+++ Task47/Program.cs
@@ -0,0 +1,21 @@
+﻿// 47. Задайте двумерный массив размером m×n, 
+//заполненный случайными вещественными числами.
+
+void InputMatrix(double[, ] matrix)
+{
+    for (double i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (double j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = new Random().NextDouble(1, 11);
+            Console.Write($"{matrix[i, j]} \t");
+        }
+        Console.WriteLine();
+    }
+}
+
+Console.Clear();
+Console.Write("Введите размеры матрицы: ");
+string[] numbers = Console.ReadLine().Split(" ");
+double[,] matrix = new double[double.Parse(numbers[0]), double.Parse(numbers[1])];
+InputMatrix(matrix);
