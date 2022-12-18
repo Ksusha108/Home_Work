5395c661f36b9c63d6eef9c425b074efe282d055
ksu <ksush3@mail.ru>
2022-12-18 20:49:05 +0300

    add task 47

--- Task47/Program.cs
+++ Task47/Program.cs
@@ -3,12 +3,12 @@
 
 void InputMatrix(double[, ] matrix)
 {
-    for (double i = 0; i < matrix.GetLength(0); i++)
+    for (int i = 0; i < matrix.GetLength(0); i++)
     {
-        for (double j = 0; j < matrix.GetLength(1); j++)
+        for (int j = 0; j < matrix.GetLength(1); j++)
         {
             matrix[i, j] = new Random().Next(1, 11); 
-            Console.Write(Math.Sqrt(matrix[i, j]));
+            Console.Write($"{matrix[i, j]} \t");
         }
         Console.WriteLine();
     }
