2579518762b312efcc1140ff1c73646e3d1f8452
ksu <ksush3@mail.ru>
2022-12-26 15:27:54 +0300

    add task66

---
+++ Task66/Program.cs
@@ -0,0 +1,17 @@
+﻿// 66: Задайте значения M и N. Напишите программу, которая найдёт 
+//сумму натуральных элементов в промежутке от M до N.
+
+int rec(int n, int m)
+{
+    
+    if (n == m)
+        return m;
+    return rec(n, m + 1) + m;
+}
+
+Console.Clear();
+Console.WriteLine("Введите значение n: ");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите значение m: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(rec(n, m));
